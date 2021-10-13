8c76066a1ff2bbd43be7546a5b4ab152684cbb7a
Max Körbächer <m.koerbaecher@reply.de>
2021-10-13 12:41:59 +0200

    fix env

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo ./aws/install
 aws --version
 
 aws configure set region eu-central-1 --profile max
-aws configure set aws_access_key_id ${{ secrets.ACCESS_KEY }} --profile max
-aws configure set aws_secret_access_key ${{ secrets.SECRET_ACCESS_KEY }} --profile max
+aws configure set aws_access_key_id $ACCESS_KEY --profile max
+aws configure set aws_secret_access_key $SECRET_ACCESS_KEY --profile max
 
 export AWS_PROFILE=max
